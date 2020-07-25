953feab9a37ea969e97336be388af0c4f6ce092c
Akhil Gandhi <akhilgandhi@live.in>
2020-07-25 23:36:09 +0530

    Spring Batch Configuration without using CommandLineJobRunner

---
+++ src/main/java/com/example/batch/listener/JobCompletionNotificationListener.java
@@ -0,0 +1,4 @@
+package com.example.batch.listener;
+
+public class JobCompletionNotificationListener {
+}
---
+++ src/main/java/com/example/batch/processor/BatchDataItemProcessor.java
@@ -0,0 +1,4 @@
+package com.example.batch.processor;
+
+public class BatchDataItemProcessor {
+}
